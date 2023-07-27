dd36fc8518c5e1f02133800700f2cf46eb9d49bb
Karl Holzmann <karl.holzmann@students.mq.edu.au>
2023-07-28 02:25:23 +1000

    file uploading

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from urllib.request import Request
 from sanic import Sanic
 from sanic.response import file, text
-from sanic.request import Request
+from sanic.request import Request, File
 import asyncio
+import aiofiles
 
 app = Sanic(name="badge");
 
@@ -15,12 +16,14 @@ async def upload_image(req: Request):
     await req.receive_body()
     if (len(req.files) == 0):
         raise NotImplementedError("no files")
-    raise NotImplementedError("got files")
-    ...
+    file: File = req.files["file"][0];
+    async with aiofiles.open("./uploads/" + file.name, "wb") as f:
+        await f.write(file.body)
+    return text("done")
 
 async def main():
     # inky init
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
     ...
 
 if __name__ == "__main__":
