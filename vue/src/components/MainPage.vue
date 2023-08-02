16b72f19a1ddb6e75eacbb1fab2c87134c787767
crpz1 <8588315+crpz1@users.noreply.github.com>
2023-08-03 01:41:49 +1000

    better fading

--- vue/src/components/MainPage.vue
+++ vue/src/components/MainPage.vue
@@ -1,7 +1,7 @@
 <template>
  <NavigationBar></NavigationBar>
  <v-progress-linear :active="isActive" height="4" indeterminate></v-progress-linear>
-  <v-container class="fill-height">
+  <v-container :class="fade.join(' ')">
     <v-responsive class="align-center text-center fill-height">
       <v-card class="mx-auto" v-for="image in images" :key="image" style="margin-bottom: 1em">
         <v-img v-ripple :src="'http://localhost:8000' + image.path" cover @click.stop="selectImage"></v-img>
@@ -18,39 +18,46 @@
 
 <script setup>
   import NavigationBar from './NavigationBar.vue'; 
+  import '../assets/styles.css';
   //
 </script>
 
 <script>
   import { ref } from "vue";
   let loading = ref(false);
+  let imageObject = ref({});
+  let fade = ref(["fill-height"]);
 
   function selectImage(a) {
     if (loading.value) return;
     loading.value = true
+    fade.value.push("fade-animation");
     fetch("http://hyperdeath.local:8000/pick_image", {
       method: "POST",
       body: JSON.stringify({image: a.currentTarget.parentElement.children[2].innerText}),
       mode: "no-cors"
-    }).then(() => loading.value = false)
+    }).then(() => {loading.value = false; fade.value.pop();})
   }
 
   function deleteImage(a) {
     if (loading.value) return;
-    loading.value = true
-    fetch(a.currentTarget.parentElement.children[2].innerText, {
+    loading.value = true;
+    fade.value.push("fade-animation");
+    fetch("http://hyperdeath.local:8000" + a.currentTarget.parentElement.parentElement.children[2].innerText, {
       method: "DELETE"
     }).then(fetch("http://hyperdeath.local:8000/enumerate_images").then(res => res.json()).then(obj => {
-      this.images = obj;
+      imageObject.value = obj;
       loading.value = false;
+      fade.value.pop();
     }))
   }
 
   export default {
     data: () => ({
-      images: {},
+      images: imageObject,
       group: null,
-      isActive: loading
+      isActive: loading,
+      fade
     }),
 
     watch: {
